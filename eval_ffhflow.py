d93a76500a83fe50a408c8afe1d1bbe91a99e024
Qian Feng <qian.feng@agile-robots.com>
2023-09-14 23:29:00 +0200

    feat: add joint conf to maad metrics

--- eval_ffhflow.py
+++ eval_ffhflow.py
@@ -1,7 +1,6 @@
 import argparse
 import torch
 import os
-import numpy as np
 
 from ffhflow.configs import get_config
 from ffhflow.datasets import FFHDataModule
@@ -11,9 +10,9 @@ from ffhflow.utils.metrics import maad_for_grasp_distribution
 from ffhflow.utils.grasp_data_handler import GraspDataHandlerVae
 
 parser = argparse.ArgumentParser(description='Probabilistic skeleton lifting training code')
-parser.add_argument('--model_cfg', type=str, default='models/ffhflow_bpsmlp_normal_flow_pos_enc_localinn_data_norm_both_transl_rot_with_joint_conf/hparams.yaml', help='Path to config file')
+parser.add_argument('--model_cfg', type=str, default='models/ffhflow_bpsmlp_normal_flow_pos_enc_glow_8layers/hparams.yaml', help='Path to config file')
 parser.add_argument('--root_dir', type=str, default='checkpoints', help='Directory to save logs and checkpoints')
-parser.add_argument('--ckpt_path', type=str, default='models/ffhflow_bpsmlp_normal_flow_pos_enc_localinn_data_norm_both_transl_rot_with_joint_conf/epoch=16-step=199955.ckpt', help='Directory to save logs and checkpoints')
+parser.add_argument('--ckpt_path', type=str, default='models/ffhflow_bpsmlp_normal_flow_pos_enc_glow_8layers/epoch=8-step=62687.ckpt', help='Directory to save logs and checkpoints')
 
 args = parser.parse_args()
 
@@ -49,17 +48,31 @@ base_path = '/home/yb/Documents/ffhflow_grasp'
 # MAAD Metrics
 grasp_data_path = os.path.join(cfg.DATASETS.PATH, cfg.DATASETS.GRASP_DATA_NANE)
 grasp_data = GraspDataHandlerVae(grasp_data_path)
+
+transl_loss_sum = 0
+rot_loss_sum = 0
+joint_loss_sum = 0
+print(len(val_loader))
 with torch.no_grad():
     for i, batch in enumerate(val_loader):
-        out = model.sample(batch['bps_object'][0], num_samples=100)
+        for idx in range(len(batch['obj_name'])):
+            palm_poses, joint_confs, num_pos = grasp_data.get_grasps_for_object(obj_name=batch['obj_name'][idx],outcome='positive')
+            grasps_gt = val_dataset.get_grasps_from_pcd_path(batch['pcd_path'][idx])
 
-        palm_poses, joint_confs, num_pos = grasp_data.get_grasps_for_object(obj_name=batch['obj_name'][0],outcome='positive')
-        grasps_gt = val_dataset.get_grasps_from_pcd_path(batch['pcd_path'][0])
+            # out = model.sample(batch['bps_object'][idx], num_samples=grasps_gt['rot_matrix'].shape[0])
+            out = model.sample(batch['bps_object'][idx], num_samples=100)
 
-        maad_for_grasp_distribution(out, grasps_gt)
-        model.show_grasps(batch['pcd_path'][0], out, i)
-        # # filtered_out = model.sort_and_filter_grasps(out, perc=0.5)
-        # # model.show_grasps(batch['pcd_path'][0], filtered_out, i+100)
-        # filtered_out = model.sort_and_filter_grasps(out, perc=0.1, return_arr=False)
-        # # model.show_grasps(batch['pcd_path'][0], filtered_out, i+200, base_path, save=False)
-        model.show_gt_grasps(batch['pcd_path'][0], grasps_gt, i+300)
+            transl_loss, rot_loss, joint_loss = maad_for_grasp_distribution(out, grasps_gt)
+            transl_loss_sum += transl_loss
+            rot_loss_sum += rot_loss
+            joint_loss_sum += joint_loss
+            # model.show_grasps(batch['pcd_path'][0], out, i)
+            # # filtered_out = model.sort_and_filter_grasps(out, perc=0.5)
+            # # model.show_grasps(batch['pcd_path'][0], filtered_out, i+100)
+            # filtered_out = model.sort_and_filter_grasps(out, perc=0.1, return_arr=False)
+            # # model.show_grasps(batch['pcd_path'][0], filtered_out, i+200, base_path, save=False)
+            # model.show_gt_grasps(batch['pcd_path'][0], grasps_gt, i+300)
+        break
+    print('transl_loss_sum:', transl_loss_sum)
+    print('rot_loss_sum:', rot_loss_sum)
+    print('joint_loss_sum:', joint_loss_sum)
