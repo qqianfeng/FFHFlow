73d3eae7f07a1a682299c4c89be76f3fb1ca2f01
Qian Feng <qian.feng@agile-robots.com>
2023-09-26 16:12:56 +0200

    feat: add clip grad for training affine coupling layer

---
+++ ffhflow/utils/train_utils.py
@@ -0,0 +1,13 @@
+import torch.nn.utils as utils
+
+
+def clip_grad_norm(optimizer, max_norm, norm_type=2):
+    """Clip the norm of the gradients for all parameters under `optimizer`.
+
+    Args:
+        optimizer (torch.optim.Optimizer):
+        max_norm (float): The maximum allowable norm of gradients.
+        norm_type (int): The type of norm to use in computing gradient norms.
+    """
+    for group in optimizer.param_groups:
+        utils.clip_grad_norm_(group['params'], max_norm, norm_type)
