fdd30bc55b160317f5cfea53c5ad04a180fb9541
Jianxiang Feng <jianxiang.feng@ar-lx0217.agile-robots.de>
2024-01-08 13:59:51 +0100

    corrected paths

--- eval_ffhflow_normflows.py
+++ eval_ffhflow_normflows.py
@@ -18,7 +18,7 @@ def load_batch(path):
 
 parser = argparse.ArgumentParser(description='Probabilistic skeleton lifting training code')
 parser.add_argument('--model_cfg', type=str, default='checkpoints/normflow_affine_old_best_param/hparams.yaml', help='Path to config file')
-parser.add_argument('--root_dir', type=str, default='checkpoints', help='Directory to save logs and checkpoints')
+# parser.add_argument('--root_dir', type=str, default='checkpoints', help='Directory to save logs and checkpoints')
 parser.add_argument('--ckpt_path', type=str, default='checkpoints/normflow_affine_old_best_param/epoch=24-step=299999.ckpt', help='Directory to save logs and checkpoints')
 
 args = parser.parse_args()
@@ -69,31 +69,36 @@ with torch.no_grad():
     print('joint_loss_sum:', joint_loss_sum)
 ###########################
 
+def pth_correction(old_pth):
+    new_path = old_pth.replace("/data/hdd1/qf/hithand_data/ffhnet-data/eval/pcd/", "/data/net/userstore/qf/hithand_data/data/ffhnet-data/eval/pcd/")
+    return new_path
+
 #### VISUALIZATION #####
-print(len(val_loader))
-with torch.no_grad():
-    batch = load_batch('eval_batch.pth')
-    for idx in range(len(batch['obj_name'])):
-        # if idx < 5:
-        #     continue
-        palm_poses, joint_confs, num_pos = grasp_data.get_grasps_for_object(obj_name=batch['obj_name'][idx],outcome='positive')
-        grasps_gt = val_dataset.get_grasps_from_pcd_path(batch['pcd_path'][idx])
+# print(len(val_loader))
+# with torch.no_grad():
+#     batch = load_batch('eval_batch.pth')
+#     for idx in range(len(batch['obj_name'])):
+#         # if idx < 5:
+#         #     continue
+#         palm_poses, joint_confs, num_pos = grasp_data.get_grasps_for_object(obj_name=batch['obj_name'][idx],outcome='positive')
+#         grasps_gt = val_dataset.get_grasps_from_pcd_path(batch['pcd_path'][idx])
 
-        # out = model.sample(batch['bps_object'][idx], num_samples=grasps_gt['rot_matrix'].shape[0])
-        out = model.sample(batch['bps_object'][idx], num_samples=100)
+#         # out = model.sample(batch['bps_object'][idx], num_samples=grasps_gt['rot_matrix'].shape[0])
+#         out = model.sample(batch['bps_object'][idx], num_samples=100)
 
-        # If we need to save the results for FFHEvaluator
-        # with open('flow_grasps.pkl', 'wb') as fp:
-        #     pickle.dump(out, fp, protocol=2)
-        # with open('data.pkl', 'wb') as fp:
-        #     pickle.dump([batch['bps_object'][idx], batch['pcd_path'][idx], batch['obj_name'][idx]], fp, protocol=2)
+#         # If we need to save the results for FFHEvaluator
+#         # with open('flow_grasps.pkl', 'wb') as fp:
+#         #     pickle.dump(out, fp, protocol=2)
+#         # with open('data.pkl', 'wb') as fp:
+#         #     pickle.dump([batch['bps_object'][idx], batch['pcd_path'][idx], batch['obj_name'][idx]], fp, protocol=2)
 
-        # model.show_grasps(batch['pcd_path'][idx], out, idx)
-        filtered_out = model.sort_and_filter_grasps(out, perc=0.5)
-        model.show_grasps(batch['pcd_path'][0], filtered_out, idx+100)
-        # filtered_out = model.sort_and_filter_grasps(out, perc=0.1, return_arr=False)
-        # # model.show_grasps(batch['pcd_path'][0], filtered_out, i+200, save_path, save=False)
-        # model.show_gt_grasps(batch['pcd_path'][idx], grasps_gt, idx+300)
+#         # model.show_grasps(batch['pcd_path'][idx], out, idx)
+#         filtered_out = model.sort_and_filter_grasps(out, perc=0.5)
+#         corrected_pth = pth_correction(batch['pcd_path'][0])
+#         model.show_grasps(corrected_pth, filtered_out, idx+100)
+#         # filtered_out = model.sort_and_filter_grasps(out, perc=0.1, return_arr=False)
+#         # # model.show_grasps(batch['pcd_path'][0], filtered_out, i+200, save_path, save=False)
+#         # model.show_gt_grasps(batch['pcd_path'][idx], grasps_gt, idx+300)
 
 
 # #### VISUALIZATION for POS + Neg Grasps#####
