e2e00e601febd3f9c870c48f3f19e8b94c911fd2
qian.feng <qian.feng@agile-robots.de>
2024-09-10 16:04:32 +0200

    add prob in sample_in_experiement

--- ffhflow/ffhflow_lvm.py
+++ ffhflow/ffhflow_lvm.py
@@ -427,7 +427,7 @@ class FFHFlowLVM(Metaclass):
             return latent_prior_ll
         elif score_type == "neg_var":
             return -cond_logvar
-        
+
     def sample(self, batch, idx, num_samples, posterior_score=None):
         """ generate number of grasp samples
 
@@ -488,7 +488,7 @@ class FFHFlowLVM(Metaclass):
 
         return output
 
-    def sample_in_experiment(self, bps, num_samples, return_cond_feat=False):
+    def sample_in_experiment(self, bps, num_samples, return_cond_feat=False, posterior_score=None):
         """ generate number of grasp samples for experiment, where each inference takes only one bps
 
         Args:
@@ -510,6 +510,7 @@ class FFHFlowLVM(Metaclass):
         self.prior_flow.to('cuda')
         self.pcd_enc.to('cuda')
         self.flow.to('cuda')
+        self.posterior_nn.to('cuda')
 
         time1 = time()
         # extract pcd feats
@@ -523,6 +524,12 @@ class FFHFlowLVM(Metaclass):
         print('prior_flow takes:',time3-time2)
         # z -> grasp
         log_prob, pred_angles, pred_pose_transl, pred_joint_conf = self.flow(conditioning_feats, num_samples=1)
+
+        if posterior_score is not None:
+            pred_grasps = torch.cat([pred_angles, pred_pose_transl, pred_joint_conf.squeeze(1)], dim=1)
+            log_prob = self.posterior_score(pcd_feats, pred_grasps, score_type=posterior_score)
+
+
         print('grasp flow takes:',time()-time3)
         print('ffhflow in total takes:',time()-time1)
         log_prob = log_prob.view(-1)
